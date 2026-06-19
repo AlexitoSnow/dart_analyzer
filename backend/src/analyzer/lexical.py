8a6d73f21ed6d3c62029fec8f7f4c70ecbd3742f
sizaguir <138946181+sizaguir@users.noreply.github.com>
2026-06-19 15:39:28 -0500

    Add reserved words and update token definitions

--- backend/src/analyzer/lexical.py
+++ backend/src/analyzer/lexical.py
@@ -1,10 +1,49 @@
 import ply.lex as lex
 
-tokens = ('ID', 'DELIMITER', 'COMMENT')
+tokens = (
+    # Identificadores
+    'ID',
+    
+    # Palabras Reservadas
+    'VAR', 'FINAL', 'CONST',
+    'IF', 'ELSE', 'RETURN',
+    
+    # Tipos de datos 
+    'INT', 'MAP',
+    
+    # Operador de asignación
+    'ASSIGN',
+)
+
+# PALABRAS RESERVADAS - DESARROLLADOR 1
+reserved = {
+    'var': 'VAR',
+    'final': 'FINAL',
+    'const': 'CONST',
+    'if': 'IF',
+    'else': 'ELSE',
+    'return': 'RETURN',
+    'int': 'INT',
+    'Map': 'MAP',
+}
+# PALABRAS RESERVADAS - DESARROLLADOR 1
+
 log = ''
 
 t_DELIMITER = r'[\(\);]\w*'
+#
 t_ID = r'[a-zA-Z_]\w*'
+
+# OPERADOR DE ASIGNACIÓN - DESARROLLADOR 1
+
+t_ASSIGN = r'='
+
+# REGLAS PARA IDENTIFICADORES - DESARROLLADOR 1 
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+# FIN DESARROLLADOR 1
 
 def t_COMMENT(t):
     r'//.*'
